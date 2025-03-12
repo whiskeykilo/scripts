da27012d84056432d3f8a779e0558503ee48afcd
Will Kapcio <18176030+whiskeykilo@users.noreply.github.com>
2025-03-11 21:59:45 -0400

    Replace npm with pnpm for global package updates and comment out macOS and App Store update sections

--- update.sh
+++ update.sh
@@ -40,37 +40,37 @@ rm ~/Brewfile;
 # Node
 # https://www.npmjs.com
 echo "Updating global node packages..."
-npm update -g > /dev/null;
+pnpm update -g > /dev/null;
 
 # Project Discovery Tool Manager (PDTM)
 # https://github.com/projectdiscovery/pdtm
 echo "Updating Project Discovery tools..."
 pdtm -ua > /dev/null 2>&1;
 
-# Update Mac App Store apps
-# https://github.com/mas-cli/mas
-echo "Upgrading macOS App Store apps..."
-mas_upgrade=$(mas upgrade 2>&1 | grep Everything)
+# # Update Mac App Store apps
+# # https://github.com/mas-cli/mas
+# echo "Upgrading macOS App Store apps..."
+# mas_upgrade=$(mas upgrade 2>&1 | grep Everything)
 
-if [ "$mas_upgrade" != "Everything is up-to-date" ]; then
-	echo "Upgrading App Store apps..."
-else
-	echo "No App Store updates available."
-fi
+# if [ "$mas_upgrade" != "Everything is up-to-date" ]; then
+# 	echo "Upgrading App Store apps..."
+# else
+# 	echo "No App Store updates available."
+# fi
 
-# macOS
-echo "Checking macOS updates..."
-mac_update=$(softwareupdate -l 2>&1 | head -1)
+# # macOS
+# echo "Checking macOS updates..."
+# mac_update=$(softwareupdate -l 2>&1 | head -1)
 
-if [ "$mac_update" != "No new software available." ]; then
-	echo "Updates found."
-	echo "Do you wish to install these updates? This will restart your Mac."
-	select yn in "Yes" "No"; do
-	    case $yn in
-		Yes ) echo "Downloading and installing macOS packages..."; softwareupdate -iaR | tail +5; break;;
-		No ) exit;;
-	    esac
-	done
-else
-	echo "No macOS updates available."
-fi
+# if [ "$mac_update" != "No new software available." ]; then
+# 	echo "Updates found."
+# 	echo "Do you wish to install these updates? This will restart your Mac."
+# 	select yn in "Yes" "No"; do
+# 	    case $yn in
+# 		Yes ) echo "Downloading and installing macOS packages..."; softwareupdate -iaR | tail +5; break;;
+# 		No ) exit;;
+# 	    esac
+# 	done
+# else
+# 	echo "No macOS updates available."
+# fi
